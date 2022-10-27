c93a6a85ef7ab4d457b0f8f0a50810d4473a83a1
thanhnhile <marrythanhnhipl205@gmail.com>
2022-10-27 18:04:09 +0700

    config axios

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+import * as request from '~/utils/request';
 
 const Menu = () => {
+    const [categories, setCategories] = useState([]);
+    const [error, setError] = useState('');
+    useEffect(() => {
+        const getAllCategories = async () => {
+            try {
+                const cates = await request.get('/categories');
+                console.log(cates)
+                setCategories(cates.data)
+            } catch (error) {
+                setError(error.message)
+            }
+        }
+        getAllCategories();
+    }, [])
     return (
-        <div>Menu</div>
+        <div className='Menu'>
+            <h1>Menu</h1>
+            {error && <p className='red'>{error}</p>}
+            <ul>
+                {
+                    categories.map((cate, index) =>
+                        <li key={index}>{cate.name}</li>
+                    )
+                }
+            </ul>
+        </div>
     )
 }
 
