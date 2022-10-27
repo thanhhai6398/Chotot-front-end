34c7feb7b9a2b6eec9f7c72fefa4051ed9f956bd
thanhnhile <marrythanhnhipl205@gmail.com>
2022-10-27 16:25:55 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-import React, { Fragment } from 'react';
-import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import { publicRoute } from '~/routes';
 import './App.css';
 
 function App() {
   return (
-    <Router>
+    <div className='App'>
       <Routes>
-        <Fragment>
-          {
-            publicRoute.map((route, index) => {
-              const Page = route.component;
-              const Layout = route?.layout || React.Fragment
-              return <Route key={index} path={route.path}
-                element={
-                  <Layout>
-                    <Page />
-                  </Layout>
-                } />
-            })
-          }
-        </Fragment>
+        {
+          publicRoute.map((route, index) => {
+            const Page = route.component;
+            const Layout = route?.layout || React.Fragment
+            return <Route key={index} path={route.path}
+              element={
+                <Layout>
+                  <Page />
+                </Layout>
+              } />
+          })
+        }
       </Routes>
-    </Router>
+    </div>
 
   );
 }
