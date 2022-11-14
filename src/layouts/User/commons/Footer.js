25833e4962f6bde5a6898293c898c621f8249a31
teddy6793 <teddytran67@gmail.com>
2022-11-15 02:10:00 +0700

    header footer landing page

--- src/layouts/User/commons/Footer.js
+++ src/layouts/User/commons/Footer.js
@@ -1,8 +1,52 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
     return (
-        <div className='bg-black'>Footer</div>
+        <div className='bg-amber-400  p-6'>
+        <div className="bg-amber-400 flex  w-full  border-b-2 border-orange-800">
+            <div className='flex-1 w-3/5 ml-12 my-5 '>
+                <div className="flex flex-col">
+                        <h6 className="text-3xl text-orange-700">CHỢ ĐỒ CŨ</h6>
+                        <h6 className="text-xs">Chợ cho mọi người</h6>
+                </div>
+                <div className="text-xs mt-5">
+                    <div>CÔNG TY TNHH MERN VIETNAM | Mã Số Thuế: ABCXYZA | Văn Phòng: Tầng 5 Tòa Nhà Trung Tâm</div>
+                    <div>Số 1 Võ Văn Ngân, Phường Linh Chiểu, TP. Thủ Đức, TP. HCM</div>
+                </div>
+                <div className='px-4'>
+
+                </div>
+            </div>
+            <div className='flex-1 w-1/5 ml-12 my-6'>
+                <div className='mb-2 text-orange-600'> LIÊN KẾT</div>
+                <div className="text-xs mt-3">
+                    <div><Link to="">Youtube</Link></div>
+                    <div><Link to="">Facebook</Link></div>
+                    <div><Link to="">Instagram</Link></div>
+                    <div><Link to="">Tiktok</Link></div>
+                </div>
+            </div>
+            <div className='flex-1 w-1/5 my-6 '>
+                <div className='mb-2 text-orange-600'>LIÊN HỆ</div>
+                <div className="text-xs mt-3">
+                    <div >E-mail: mern@gmail.com</div>
+                    <div>Hotline: 0972358764</div>
+                    <div>Địa chỉ: 484 Lê Văn Việt, Tăng Nhơn Phú A, TP.Thủ Đức, TP.HCM</div>
+                </div>
+            </div>
+            <div className='flex-1 w-1/5 my-6 ml-10'>
+                <div className='mb-2 text-orange-600'>CHÍNH SÁCH</div>
+                <div className="text-xs mt-3">
+                    <div>Chính sách đổi trả</div>
+                    <div>Chính sách bảo hành</div>
+                    <div>Chính sách bảo mật</div>
+                    <div>Các câu hỏi thường gặp</div>
+                </div>
+            </div>
+            
+        </div>
+        </div>
     )
 }
 
--- src/layouts/User/commons/Header.js
+++ src/layouts/User/commons/Header.js
@@ -1,8 +1,24 @@
-import React from 'react'
-
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { AiOutlineHome, AiOutlineDatabase, AiOutlineHeart,AiOutlineUser,AiOutlineKey } from "react-icons/ai";
 const Header = () => {
   return (
-    <div className='bg-yellow-500'>Header</div>
+    <div className='bg-amber-400 '>
+      <div className='flex flex-row py-2'>
+        <Link to="/" className ="font-roboto font-bold text-5xl text-white ml-72">Chợ Đồ Cũ</Link>
+        <Link to = "/" className ="flex flex-row mt-3 ml-12 mr-9 text-2xl"><AiOutlineHome/> Trang chủ</Link>
+        <Link to = "/" className ="flex flex-row mt-3 mx-9 text-2xl"><AiOutlineDatabase/>Quản lý tin</Link>
+        <Link to = "/" className ="flex flex-row mt-3 mx-9 text-2xl"><AiOutlineHeart/>Tin theo dõi</Link>
+      </div>
+      <div className='flex flex-row py-2'>
+        <input className='border-white rounded px-5 ml-96 w-80 h-10' placeholder=' Tìm kiếm sản phẩm'></input>
+        <button className='bg-orange-500 rounded px-5 ml-1 w-14 h-10 align-middle'><AiOutlineKey/> </button>
+
+        <Link to="" className='flex flex-row text-center ml-24 px-5 pt-1 text-lg'><AiOutlineUser/> Đăng nhập</Link>
+        
+        <Link to="" className='bg-orange-500 rounded w-32 h-10 px-5 pt-1 text-center text-lg'>Đăng tin</Link>
+      </div>    
+    </div>
   )
 }
 
