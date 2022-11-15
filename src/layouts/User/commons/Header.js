09165c57cadec66823ebd9bde04f4ea6e08a8b34
thanhnhile <marrythanhnhipl205@gmail.com>
2022-11-15 12:03:54 +0700

    fix header

--- src/layouts/User/commons/Header.js
+++ src/layouts/User/commons/Header.js
@@ -1,23 +1,75 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { AiOutlineHome, AiOutlineDatabase, AiOutlineHeart,AiOutlineUser,AiOutlineKey } from "react-icons/ai";
+import React from 'react'
+import { Link } from 'react-router-dom'
+import {
+  AiOutlineHome,
+  AiOutlineDatabase,
+  AiOutlineHeart,
+  AiOutlineUser,
+  AiOutlineSearch,
+} from 'react-icons/ai'
 const Header = () => {
+  const NAV_LINKS = [
+    {
+      text: 'Trang chủ',
+      icon: <AiOutlineHome />,
+      path: '/',
+    },
+    {
+      text: 'Quản lý tin',
+      icon: <AiOutlineDatabase />,
+      path: '/',
+    },
+    {
+      text: 'Tin theo dõi',
+      icon: <AiOutlineHeart />,
+      path: '/',
+    },
+  ]
+  const handleNavLinks = () => {
+    return NAV_LINKS.map((link, index) => {
+      return (
+        <li key={index}>
+          <Link to={link.path} className="flex flex-row items-center text-lg">
+            <span className="text-2xl mr-2">{link.icon}</span>
+            {link.text}
+          </Link>
+        </li>
+      )
+    })
+  }
   return (
-    <div className='bg-amber-400 '>
-      <div className='flex flex-row py-2'>
-        <Link to="/" className ="font-roboto font-bold text-5xl text-white ml-72">Chợ Đồ Cũ</Link>
-        <Link to = "/" className ="flex flex-row mt-3 ml-12 mr-9 text-2xl"><AiOutlineHome/> Trang chủ</Link>
-        <Link to = "/" className ="flex flex-row mt-3 mx-9 text-2xl"><AiOutlineDatabase/>Quản lý tin</Link>
-        <Link to = "/" className ="flex flex-row mt-3 mx-9 text-2xl"><AiOutlineHeart/>Tin theo dõi</Link>
+    <div className="bg-amber-400 mx-auto max-w-5xl lg:max-w-7xl lg:px-8 py-3">
+      <div className="flex">
+        <Link
+          to="/"
+          className="font-roboto font-bold text-4xl mr-16 text-white"
+        >
+          Chợ Đồ Cũ
+        </Link>
+        <ul className="flex flex-row py-2 gap-10">{handleNavLinks()}</ul>
       </div>
-      <div className='flex flex-row py-2'>
-        <input className='border-white rounded px-5 ml-96 w-80 h-10' placeholder=' Tìm kiếm sản phẩm'></input>
-        <button className='bg-orange-500 rounded px-5 ml-1 w-14 h-10 align-middle'><AiOutlineKey/> </button>
+      <div className="flex flex-row justify-between mt-2">
+        <input
+          className="border-white rounded px-5 w-80 h-10 flex-1"
+          placeholder=" Tìm kiếm sản phẩm"
+        ></input>
+        <button className="bg-orange-500 rounded px-5 ml-1 w-14 h-10 text-center align-middle">
+          <AiOutlineSearch className="text-2xl text-white" />{' '}
+        </button>
+        <Link
+          to=""
+          className="flex flex-row items-center text-center ml-24 px-5 text-lg"
+        >
+          <AiOutlineUser className="mr-3 text-2xl" /> Đăng nhập
+        </Link>
 
-        <Link to="" className='flex flex-row text-center ml-24 px-5 pt-1 text-lg'><AiOutlineUser/> Đăng nhập</Link>
-        
-        <Link to="" className='bg-orange-500 rounded w-32 h-10 px-5 pt-1 text-center text-lg'>Đăng tin</Link>
-      </div>    
+        <Link
+          to=""
+          className="bg-orange-500 rounded w-32 h-10 px-5 pt-1 text-center text-lg"
+        >
+          Đăng tin
+        </Link>
+      </div>
     </div>
   )
 }
