bc452c18d986f7494b3bcd8c375b1f795f36466b
thanhnhile <marrythanhnhipl205@gmail.com>
2022-11-19 21:36:21 +0700

    mange post

--- src/layouts/User/commons/Footer.js
+++ src/layouts/User/commons/Footer.js
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom'
 
 const Footer = () => {
     return (
-        <div className='bg-amber-400  p-6'>
-        <div className="bg-amber-400 flex  w-full  border-b-2 border-orange-800">
-            <div className='flex-1 w-3/5 ml-12 my-5 '>
-                <div className="flex flex-col">
-                        <h6 className="text-3xl text-orange-700">CHỢ ĐỒ CŨ</h6>
+        <div className='bg-gray-800 text-white  mx-auto max-w-5xl lg:max-w-7xl lg:px-8'>
+        <div className="flex py-6  justify-between">
+            <div className='w-2/5 px-2'>
+                <div className="mb-5">
+                        <h6 className="text-3xl text-primary cursor-pointer py-1">CHỢ ĐỒ CŨ</h6>
                         <h6 className="text-xs">Chợ cho mọi người</h6>
                 </div>
-                <div className="text-xs mt-5">
+                <div className="text-sm">
                     <div>CÔNG TY TNHH MERN VIETNAM | Mã Số Thuế: ABCXYZA | Văn Phòng: Tầng 5 Tòa Nhà Trung Tâm</div>
                     <div>Số 1 Võ Văn Ngân, Phường Linh Chiểu, TP. Thủ Đức, TP. HCM</div>
                 </div>
@@ -18,33 +18,32 @@ const Footer = () => {
 
                 </div>
             </div>
-            <div className='flex-1 w-1/5 ml-12 my-6'>
-                <div className='mb-2 text-orange-600'> LIÊN KẾT</div>
-                <div className="text-xs mt-3">
-                    <div><Link to="">Youtube</Link></div>
-                    <div><Link to="">Facebook</Link></div>
-                    <div><Link to="">Instagram</Link></div>
-                    <div><Link to="">Tiktok</Link></div>
-                </div>
+            <div className='w-1/5 px-2'>
+                <div className='mb-2'> LIÊN KẾT</div>
+                <ul className="font-light">
+                    <li><Link to="">Youtube</Link></li>
+                    <li><Link to="">Facebook</Link></li>
+                    <li><Link to="">Instagram</Link></li>
+                    <li><Link to="">Tiktok</Link></li>
+                </ul>
             </div>
-            <div className='flex-1 w-1/5 my-6 '>
-                <div className='mb-2 text-orange-600'>LIÊN HỆ</div>
-                <div className="text-xs mt-3">
-                    <div >E-mail: mern@gmail.com</div>
-                    <div>Hotline: 0972358764</div>
-                    <div>Địa chỉ: 484 Lê Văn Việt, Tăng Nhơn Phú A, TP.Thủ Đức, TP.HCM</div>
-                </div>
+            <div className='w-1/5 px-2'>
+                <div className='mb-2'>LIÊN HỆ</div>
+                <ul className="font-light">
+                    <li >E-mail: mern@gmail.com</li>
+                    <li>Hotline: 0972358764</li>
+                    <li>Địa chỉ: 484 Lê Văn Việt, Tăng Nhơn Phú A, TP.Thủ Đức, TP.HCM</li>
+                </ul>
             </div>
-            <div className='flex-1 w-1/5 my-6 ml-10'>
-                <div className='mb-2 text-orange-600'>CHÍNH SÁCH</div>
-                <div className="text-xs mt-3">
-                    <div>Chính sách đổi trả</div>
-                    <div>Chính sách bảo hành</div>
-                    <div>Chính sách bảo mật</div>
-                    <div>Các câu hỏi thường gặp</div>
-                </div>
+            <div className='w-1/5 px-2'>
+                <div className="mb-2">CHÍNH SÁCH</div>
+                <ul className="font-light">
+                    <li>Chính sách đổi trả</li>
+                    <li>Chính sách bảo hành</li>
+                    <li>Chính sách bảo mật</li>
+                    <li>Các câu hỏi thường gặp</li>
+                </ul>
             </div>
-            
         </div>
         </div>
     )
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,47 +1,52 @@
 import Home from '~/pages/Home'
 import PostDetail from '~/pages/PostDetail'
-import Login from '~/pages/Login';
-import Register from '~/pages/Register';
-import BlankLayout from '~/layouts/User/BlankLayout';
-import AddPost from '~/pages/AddPost';
-import Following from '~/pages/Following';
-import PostsSaved from '~/pages/PostsSaved';
+import Login from '~/pages/Login'
+import Register from '~/pages/Register'
+import BlankLayout from '~/layouts/User/BlankLayout'
+import AddPost from '~/pages/AddPost'
+import Following from '~/pages/Following'
+import PostsSaved from '~/pages/PostsSaved'
+import ManagePosts from '~/pages/ManagePosts'
 
 export const publicRoute = [
-    {
-        path: '',
-        component: Home
-    },
-    {
-        path: 'auth',
-        component: Login,
-        layout: BlankLayout
-    },
-    {
-        path: 'register',
-        component: Register,
-        layout: BlankLayout
-    },
-    {
-        path: 'addPost',
-        component: AddPost
-    },
-    {
-        path: 'posts/:id',
-        component: PostDetail
-    },
-    {
-        path: 'following',
-        component: Following
-    },
-    {
-        path: 'postsSaved',
-        component: PostsSaved
-    },
-];
+  {
+    path: '',
+    component: Home,
+  },
+  {
+    path: 'auth',
+    component: Login,
+    layout: BlankLayout,
+  },
+  {
+    path: 'register',
+    component: Register,
+    layout: BlankLayout,
+  },
+  {
+    path: 'addPost',
+    component: AddPost,
+  },
+  {
+    path: 'posts/:id',
+    component: PostDetail,
+  },
+  {
+    path: 'following',
+    component: Following,
+  },
+  {
+    path: 'postsSaved',
+    component: PostsSaved,
+  },
+  {
+    path: 'managePosts',
+    component: ManagePosts,
+  },
+]
 export const privateRoute = [
-    {
-        path: 'admin/dashboard',
-        component: '',
-    },
-];
+  {
+    path: 'admin/dashboard',
+    component: '',
+  },
+]
