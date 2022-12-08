257e8263814ae2d9e8aa30348b979c9b7b0ad2c1
teddy6793 <90394132+teddy6793@users.noreply.github.com>
2022-12-08 17:27:53 +0700

    user

---
+++ src/apiServices/userService.js
@@ -0,0 +1,10 @@
+import * as request from '~/utils/request';
+
+export const httpGetUserById = (id) => {
+    try {
+      const res = request.get(`/users/${id}`);
+      return res;
+    } catch (error) {
+      console.log(error.response.data);
+    }
+  };
